a36a49187a339459621b27415996570a8bcf7aae
Volodymyr Mykytiuk <mvlabaratory@gmail.com>
2019-04-06 21:17:36 +0300

    Add project files.

---
+++ RD_SolidPatterns/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RD_SolidPatterns.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
